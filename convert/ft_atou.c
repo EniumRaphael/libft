28be9d6b91d5313fe36f3c8f9c82ce13adeef87a
Raphael <rparodi@student.42.fr>
2025-09-08 10:21:48 +0200

    feat(convert/atou): adding the atou function
    - Can be usefull when the parsing of some project (like philosophers)

---
+++ convert/ft_atou.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atou.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rparodi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 17:22:41 by rparodi           #+#    #+#             */
+/*   Updated: 2025/09/08 10:16:01 by rparodi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+static int	ft_check_space(int c)
+{
+	if (c == 32 || (c >= 9 && c <= 13))
+		return (1);
+	return (0);
+}
+
+size_t	ft_atou(const char *nptr)
+{
+	size_t	i;
+	size_t	number;
+
+	if (!nptr)
+		return (0);
+	i = 0;
+	number = 0;
+	while (nptr[i])
+	{
+		if (nptr[i] >= '0' && nptr[i] <= '9')
+			number = (number * 10) + nptr[i] - '0';
+		else
+			break ;
+		i++;
+	}
+	return (number);
+}
